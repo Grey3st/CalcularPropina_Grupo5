03042ea8469a9123fd817806c5ff7d0f5528e511
caseresa <anacaseres221@gmail.com>
2023-09-09 15:51:44 -0300

    cambios en html + add modal + cambios en js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ function calcularPropina(){
     let totalComida = parseInt(costoComida) + parseInt(propina);
     document.getElementById("resTotal").value = totalComida;
 }
+
 function showCardInfo() {
     // Obtiene referencias a elementos HTML por su ID
     let creditCardOption = document.getElementById("creditCardOption");
@@ -17,6 +18,9 @@ function showCardInfo() {
     let cardName = document.getElementById("cardName");
     let cardImage = document.getElementById("cardImage");
     let inputTarjeta = document.getElementById("inputTarjeta");
+
+    let labelTarjeta = document.getElementById("labelTarjeta");
+    let paymentOption = document.getElementById("paymentOption"); 
 
     // Obtiene el valor seleccionado en el menú desplegable
     let selectedOption = creditCardOption.value; // Obtener la opción seleccionada en el primer menú desplegable
@@ -27,6 +31,8 @@ function showCardInfo() {
     if (selectedOption === "tarjeta") {
         // Muestra la información de la tarjeta
         cardInfo.style.display = "block";
+        labelTarjeta.style.display = "block";
+        paymentOption.style.display = "block";
 
         // Configura la información de la tarjeta seleccionada
         switch (selectedOption) {
@@ -47,6 +53,11 @@ function showCardInfo() {
 
         // Muestra el campo para el número de tarjeta
         inputTarjeta.style.display = "none";
+    }else{
+        if (selectedOption == "efectivo"){
+            labelTarjeta.style.display = "none";
+            paymentOption.style.display = "none";
+        }
     }
 }
 window.onload = function () {
@@ -54,6 +65,32 @@ window.onload = function () {
 };
 
 console.log("pagina fachera hecha con ganas y que todo el equipo trabajo epicamente GG");
+
+// Ventana modal
+var modal = document.getElementById("ventanaModal");
+
+// Botón que abre el modal
+var boton = document.getElementById("abrirModal");
+
+// Hace referencia al elemento <span> que tiene la X que cierra la ventana
+var span = document.getElementsByClassName("cerrar")[0];
+
+// Cuando el usuario hace click en el botón, se abre la ventana
+boton.addEventListener("click",function() {
+  modal.style.display = "block";
+});
+
+// Si el usuario hace click en la x, la ventana se cierra
+span.addEventListener("click",function() {
+  modal.style.display = "none";
+});
+
+// Si el usuario hace click fuera de la ventana, se cierra.
+window.addEventListener("click",function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
 
 // la wea tenia que haberla hecho con css pero como me colgue la hice con java, ke paso las pinches imagenes no cargan
 //cuestion master matias deberas hacer lo siguiente estas son tus misiones diarias :
